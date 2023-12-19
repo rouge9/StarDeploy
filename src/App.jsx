2b87d359d3247dcdd8e9ee8d63746b284a20a453
rouge9 <robelged1992@gmail.com>
2023-12-19 02:55:58 -0900

    change app route

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Projects from "./routes/Projects";
 import GalleryPage from "./routes/GalleryPage";
 import ErrorPage from "./componets/ErrorPage";
 
 function App() {
-  const routes = [
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/projects",
-      element: <Projects />,
-    },
-    {
-      path: "/gallery",
-      element: <GalleryPage />,
-    },
-    {
-      path: "*",
-      element: <ErrorPage />,
-    },
-  ];
-
-  const router = createBrowserRouter(routes, {
-    basename: import.meta.env.DEV ? "/" : "/StarDeploy/",
-  });
-
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/StarDeploy/"}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
