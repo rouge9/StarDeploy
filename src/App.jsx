b4cf3efc2408d1d6a4fd748bd25556f47d19599e
rouge9 <robelged1992@gmail.com>
2024-02-29 03:23:43 -0900

    check deploy

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
       {/* <NavBar /> */}
       {/* <FloatingNavBar /> */}
       {/* <Hero /> */}
+      {/*  */}
       <ShuffleHero />
       {/* <Divider /> */}
       <SimpleClients />
